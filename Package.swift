75f94c2a4ef163f53ffa616a27e3c6851d895f13
Junard Lebajan <junard.lebajan@gmail.com>
2023-03-31 14:06:07 -0400

    Updates package dependencies.

--- Package.swift
+++ Package.swift
@@ -5,18 +5,36 @@ import PackageDescription
 
 let package = Package(
     name: "FireworkVideoAgoraSupport",
+    platforms: [
+        .iOS(.v12)
+    ],
     products: [
         .library(
             name: "FireworkVideoAgoraSupport",
-            targets: ["FireworkVideoAgoraSupport"]
+            targets: ["FireworkVideoAgoraSupport", "FireworkVideoAgoraSupportDependencies"])
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/AgoraIO/AgoraRtcEngine_iOS.git",
+            .upToNextMajor(from: "4.1.1")
+        ),
+        .package(
+            url: "https://github.com/apple/swift-protobuf.git",
+            .upToNextMajor(from: "1.21.0")
         )
     ],
-    dependencies: [],
     targets: [
         .binaryTarget(
             name: "FireworkVideoAgoraSupport",
             url: "https://github.com/loopsocial/firework_ios_sdk_agora_support/releases/download/v0.2.0/FireworkVideoAgoraSupport-v0.2.0.xcframework.zip",
             checksum: "80b04f9c072bf748ec747110724606bfb7d92a7b552c1e4c1a187d7352ec4277"
         )
+        ,
+        .target(
+            name: "FireworkVideoAgoraSupportDependencies",
+            dependencies: [
+                .product(name: "RtcBasic", package: "AgoraRtcEngine_iOS"),
+                .product(name: "SwiftProtobuf", package: "swift-protobuf")
+            ])
     ]
 )
