35bfe355e5c58f411c774b094f5764549a374a58
Firework <loopops@users.noreply.github.com>
2025-03-23 15:09:37 +0000

    Update SDK support library to v0.6.0-beta.2

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FireworkVideoAgoraSupport",
-            url: "https://github.com/loopsocial/firework_ios_sdk_agora_support/releases/download/v0.6.0-beta.1/FireworkVideoAgoraSupport-v0.6.0-beta.1.xcframework.zip",
-            checksum: "80044b7603992a0a743dc8fcb8be6c6a41d7637e81a0c575e224b9a50a523e25"
+            url: "https://github.com/loopsocial/firework_ios_sdk_agora_support/releases/download/v0.6.0-beta.2/FireworkVideoAgoraSupport-v0.6.0-beta.2.xcframework.zip",
+            checksum: "d44dd99f57451c53e6e341cf24be12b0f91dbf99454fe60f8890d3b46950ba8a"
         )
         ,
         .target(
